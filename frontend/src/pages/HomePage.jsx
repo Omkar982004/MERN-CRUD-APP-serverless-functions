e3dcaf66f2d1492d2382a7ae56c0a798713ab13b
Omkar Lakhute <omkarlakhutework2@gmail.com>
2025-09-06 21:40:36 +0530

    me functionality in homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,17 +1,28 @@
+import { useEffect, useState } from "react";
 import Hero from '../components/Hero';
 import HomeCards from "../components/HomeCards";
 import JobListings from "../components/JobListings";
-import ViewAllJobs  from "../components/ViewAllJobs";
-
+import ViewAllJobs from "../components/ViewAllJobs";
+import { getCurrentUser } from "../api/auth";
 const HomePage = () => {
-    return (
-        <>
-            <Hero />
-            <HomeCards />
-            <JobListings isHome = {true} />
-            <ViewAllJobs />
-        </>
-    );
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await getCurrentUser();
+      if (user?.name) setUserName(user.name);
+    };
+    fetchUser();
+  }, []);
+
+  return (
+    <>
+      <Hero userName={userName} />
+      <HomeCards />
+      <JobListings isHome={true} />
+      <ViewAllJobs />
+    </>
+  );
 };
 
 export default HomePage;
